3574811d877e3438d51499c6fbcb9a570744df6d
rontysarkar <rontysarkar2023@gmail.com>
2024-06-19 01:02:44 +0600

    add hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,9 +5,13 @@ import HeroSection from "./componet/HeroSection";
 const page = () => {
   return (
     <div className="min-h-screen bg-black">
-      <div className="max-w-7xl mx-auto">
+      <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]">
+
+      
+      <div className="max-w-7xl mx-auto p-5 ">
         <Navbar/>
         <HeroSection/>
+      </div>
       </div>
     </div>
   );
